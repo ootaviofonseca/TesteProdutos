18e773de5f06aa44623ec80e23f9e73212f790ac
Otavio Fonseca <otavioaf123@gmail.com>
2024-02-20 20:44:15 -0300

    inicio do front

--- produtos/src/main/java/br/com/api/produtos/controller/produtoController.java
+++ produtos/src/main/java/br/com/api/produtos/controller/produtoController.java
@@ -7,6 +7,7 @@ import br.com.api.produtos.service.ProdutoService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 
  
 @RestController
+@CrossOrigin(origins = "*")//o '*' permite que a API seja acessada por qualquer origem
 public class produtoController {
     @Autowired
     private ProdutoService ps;
