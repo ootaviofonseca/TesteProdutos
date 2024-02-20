a329d33d45e1235ac6447b59b9e0968f2249a733
Otavio Fonseca <otavioaf123@gmail.com>
2024-02-20 14:06:04 -0300

    /listar

--- produtos/src/main/java/br/com/api/produtos/controller/produtoController.java
+++ produtos/src/main/java/br/com/api/produtos/controller/produtoController.java
@@ -1,13 +1,29 @@
 package br.com.api.produtos.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.api.produtos.modelo.ProdutoModelo;
+import br.com.api.produtos.service.ProdutoService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+
+
 
 
 
 //Cria as rotas e requisicoes como GET, POST, PUT, DELETE
 @RestController
 public class produtoController {
+    @Autowired
+    private ProdutoService ps;
+
+    @GetMapping("/listar")
+    public Iterable<ProdutoModelo>listar(){
+        return ps.listar();
+    }
+    
+
     @GetMapping("/")
     public String route(){
         return "API de produtos funcionando!";
