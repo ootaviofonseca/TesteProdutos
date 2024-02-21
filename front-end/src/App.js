5212f8edb9aa482e6b5f3beb631b5a2c427530c0
Otavio Fonseca <otavioaf123@gmail.com>
2024-02-20 22:23:52 -0300

    Front-end iniciando

---
+++ front-end/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Formulario from './Formulario';
+import Tabela from './Tabela';
+
+function App() {
+  return (
+    <div className="App">
+    <Formulario />
+    <Tabela />
+    </div>
+  );
+}
+
+export default App;
---
+++ front-end/src/Tabela.js
@@ -0,0 +1,27 @@
+function Tabela(){
+    return (
+        <table>
+        <thead> 
+            <tr>
+                <th>#</th>
+                <th>Nome</th>
+                <th>Marca</th>
+                <th>Selecionar</th>
+            </tr>
+        </thead>
+        
+        <tbody> 
+            <tr>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+            </tr>
+        </tbody>
+        
+        </table>
+    );
+    
+}
+
+export default Tabela;
